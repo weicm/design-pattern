95c60eb17d371176804d9cb15beac9c3ba222240
魏常明 <weichangming@julong.cc>
2018-05-14 09:55:46 +0800

    修改注释

--- src/main/java/cn/design/mode/strategy/CashContext.java
+++ src/main/java/cn/design/mode/strategy/CashContext.java
@@ -3,7 +3,7 @@ package cn.design.mode.strategy;
 /**
  * <p>Author: weicm</p>
  * <p>Date: 2018/3/23 14:15</p>
- * <p>Desp: 策略上下文，保存具体的策略的一你用，统一策略出口</p>
+ * <p>Desp: 策略上下文，保存具体的策略的一个引用用，统一策略出口</p>
  */
 public class CashContext {
     //具体策略
