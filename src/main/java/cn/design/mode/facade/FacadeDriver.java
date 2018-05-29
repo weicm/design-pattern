9ed3311f0392c60cfdb149b047b2f45b53c59323
魏常明 <weichangming@julong.cc>
2018-05-29 09:48:48 +0800

    增加外观模式

---
+++ src/main/java/cn/design/mode/facade/FacadeDriver.java
@@ -0,0 +1,12 @@
+package cn.design.mode.facade;
+
+/**
+ * Created by weicm on 2018/5/29.
+ */
+public class FacadeDriver {
+    public static void main(String[] args) {
+        Facade facade = new Facade();
+        facade.methodA();
+        facade.methodB();
+    }
+}
---
+++ src/main/java/cn/design/mode/facade/SubsystemTwo.java
@@ -0,0 +1,10 @@
+package cn.design.mode.facade;
+
+/**
+ * Created by weicm on 2018/5/29.
+ */
+public class SubsystemTwo {
+    public void methodTwo() {
+        System.out.println("子系统方法2");
+    }
+}
