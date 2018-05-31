b50ba8f7bae04e6608ac956736b15ed4cdc52894
魏常明 <weichangming@julong.cc>
2018-05-31 10:00:22 +0800

    添加观察者模式

---
+++ src/main/java/cn/design/mode/observer/ConcreteObserver.java
@@ -0,0 +1,29 @@
+package cn.design.mode.observer;
+
+/**
+ * Created by weicm on 2018/5/31.
+ */
+public class ConcreteObserver extends Observer {
+    private String name;
+    private ConcreteSubject subject;
+    private String observerState;
+
+    public ConcreteObserver(String name, ConcreteSubject subject) {
+        this.name = name;
+        this.subject = subject;
+    }
+
+    public String getObserverState() {
+        return observerState;
+    }
+
+    public void setObserverState(String observerState) {
+        this.observerState = observerState;
+    }
+
+    @Override
+    public void update() {
+        observerState = subject.getSubjectState();
+        System.out.println(String.format("观察者 %s 的新状态是 %s", name, observerState));
+    }
+}
---
+++ src/main/java/cn/design/mode/observer/Observer.java
@@ -0,0 +1,8 @@
+package cn.design.mode.observer;
+
+/**
+ * Created by weicm on 2018/5/31.
+ */
+public abstract class Observer {
+    public abstract void update();
+}
