3a82b09578170f36687de652e6918bac1d3ad91c
魏常明 <weichangming@julong.cc>
2018-07-03 10:04:39 +0800

    增加组合模式

---
+++ src/main/java/cn/design/mode/composite/CompositeDriver.java
@@ -0,0 +1,31 @@
+package cn.design.mode.composite;
+
+/**
+ * <p>Created by weicm on 2018/7/3 9:46</p>
+ * <p>Desp: 组合模式</p>
+ */
+public class CompositeDriver {
+    public static void main(String[] args) {
+        Composite root = new Composite("root");
+        root.add(new Leaf("Leaf A"));
+        root.add(new Leaf("Leaf B"));
+
+        Composite comp = new Composite("Composite X");
+        comp.add(new Leaf("Leaf XA"));
+        comp.add(new Leaf("Leaf XB"));
+
+        root.add(comp);
+
+        Composite comp2 = new Composite("Composite XY");
+        comp2.add(new Leaf("Leaf XYA"));
+        comp2.add(new Leaf("Leaf XYB"));
+
+        comp.add(comp2);
+
+        Leaf leaf = new Leaf("Leaf D");
+        root.add(leaf);
+        root.remove(leaf);
+
+        root.display(4);
+    }
+}
---
+++ src/main/java/cn/design/mode/composite/Leaf.java
@@ -0,0 +1,27 @@
+package cn.design.mode.composite;
+
+/**
+ * <p>Created by weicm on 2018/7/3 9:46</p>
+ * <p>Desp: 叶子结点</p>
+ * <p>&nbsp;&nbsp; 叶子结点，没有子节点，没有add、remove方法，只有display方法</p>
+ */
+public class Leaf extends Component {
+    public Leaf(String name) {
+        super(name);
+    }
+
+    @Override
+    public void add(Component component) {
+        throw new RuntimeException("不支持该操作！");
+    }
+
+    @Override
+    public void remove(Component component) {
+        throw new RuntimeException("不支持该操作！");
+    }
+
+    @Override
+    public void display(Integer depth) {
+        System.out.println(String.format("%0"+depth+"d  :  %s", 0, getName()).replaceAll("0", "-"));
+    }
+}
