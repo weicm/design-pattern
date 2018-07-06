d3b8b6737f298689392c9c6d2a5c7ccdb12a3158
魏常明 <weichangming@julong.cc>
2018-07-06 14:56:38 +0800

    增加桥接模式

---
+++ src/main/java/cn/design/mode/bridge/AddrList.java
@@ -0,0 +1,13 @@
+package cn.design.mode.bridge;
+
+/**
+ * <p>Created by weicm on 2018/7/6 14:48</p>
+ * <p>Desp: 具体小分类</p>
+ * <p>&nbsp;&nbsp; 手机通讯录</p>
+ */
+public class AddrList extends PhoneSoft{
+    @Override
+    public void run() {
+        System.out.println("运行手机通讯录");
+    }
+}
