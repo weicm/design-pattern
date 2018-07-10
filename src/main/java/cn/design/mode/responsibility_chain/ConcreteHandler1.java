ccbdd57d182a3106de9f708a3b8297b48d66fc5a
魏常明 <weichangming@julong.cc>
2018-07-10 11:50:20 +0800

    增加职责链模式

---
+++ src/main/java/cn/design/mode/responsibility_chain/ConcreteHandler1.java
@@ -0,0 +1,21 @@
+package cn.design.mode.responsibility_chain;
+
+/**
+ * <p>Created by weicm on 2018/7/10 11:39</p>
+ * <p>Desp: 具体请求处理者</p>
+ */
+public class ConcreteHandler1 extends Handler {
+
+
+    public ConcreteHandler1(String name) {
+        super(name);
+    }
+
+    @Override
+    public void handle(Integer request) {
+        if (request > 0 & request <= 10)
+            System.out.println(String.format("%s处理了请求%d", name, request));
+        else
+            successor.handle(request);
+    }
+}
